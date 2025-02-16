232040d0eb177232b494f7992a03457ff644d16f
David Sotelo <davidsotelo@masmetodo.com>
2025-02-16 17:32:38 +0100

    Actualización Ejerc5

--- Ejercicio5_2.sql
+++ Ejercicio5_2.sql
@@ -1,15 +1,27 @@
 /* query ejercicio 5 practica*/
-/* Opto por retirar en base a la interpretacion que hago de enunciado, no hay datos en la columna document_type porlo que no puedo edificarlos por este campo.Cabria la posibilidad de poder identificarlos por la columna phone_number entendiendose que son valores en relacion uno a uno con el cliente*/
+
 select 
     ivr_calls.ivr_id,
     ivr_calls.phone_number,
     ivr_calls.ivr_result,
     ivr_calls.vdn_label,
-    max(ivr_steps.document_type) as document_type,  
-    max(ivr_steps.document_identification) as document_identification  
+    coalesce(
+        max(case 
+            when ivr_steps.document_type not in ('unknown', 'desconocido') 
+            then ivr_steps.document_type 
+        end), 
+        ivr_calls.phone_number
+    ) as document_type,
+    coalesce(
+        max(case 
+            when ivr_steps.document_identification not in ('unknown', 'desconocido') 
+            then ivr_steps.document_identification 
+        end), 
+        ivr_calls.phone_number
+    ) as document_identification
+
 from keepcoding.ivr_calls
-inner
+left
 join keepcoding.ivr_steps
 on ivr_calls.ivr_id = ivr_steps.ivr_id
-where ivr_steps.document_type not in ('unknown', 'desconocido')
 group by ivr_calls.ivr_id, ivr_calls.phone_number, ivr_calls.ivr_result, ivr_calls.vdn_label;
